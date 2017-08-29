31bf35aeeeadadc57e2b95dcae4e9bcf60d5cbfb
Kella <kellador@gmail.com>
2017-08-29 14:05:50 +0200

    Some more housecleaning

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 
-setup(
-    name='Charfred',
-    version='0.1dev',
-    packages=['charfred'],
-    license='Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)'
-)
+setup(name='Charfred',
+      version='0.1dev',
+      description='Discord Bot that interacts with remote Scripts'
+      'via SSH, to aid Minecraft server administration',
+      author='Daniel Kella',
+      author_email='kellador@gmail.com',
+      packages=['charfred'],
+      license='Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)')
