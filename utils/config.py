33c959dc7c1ac26d914f713d864efff0594358ad
Kellador <kellador@gmail.com>
2019-11-02 20:34:10 +0100

    :sparkles: add support for toml in Config
    includes automatic conversion of existing config file with other format;
    Just change the suffix.

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,8 @@
 import asyncio
-import os
 import logging
 import json
 import toml
+from pathlib import Path
 from collections.abc import MutableMapping
 
 log = logging.getLogger('charfred')
@@ -12,37 +12,57 @@ class Config(MutableMapping):
     """Config MutableMapping for dynamic configuration options;
     Parses data to and from json or toml files.
     """
+
     def __init__(self, cfgfile, **opts):
-        self.cfgfile = cfgfile
+        self.cfgfile = Path(cfgfile)
         self.loop = opts.pop('loop', None)
         self.lock = asyncio.Lock()
         self.default = opts.pop('default', False)
-        if opts.pop('load', False):
-            self._load()
-        if opts.pop('toml', False):
-            self.loadfunc = json.load
-            self.dumpfunc = json.dump
-        else:
+        self.toml = True if self.cfgfile.suffix == '.toml' else False
+        if self.toml:
             self.loadfunc = toml.load
             self.dumpfunc = toml.dump
+        else:
+            self.loadfunc = json.load
+            self.dumpfunc = json.dump
+        if opts.pop('load', False):
+            self._load()
+
+    def _convert(self):
+        if self.toml:
+            convertee = self.cfgfile.with_suffix('.json')
+            if convertee.exists():
+                self.loadfunc = json.load
 
     def _load(self):
+        convertee = self.cfgfile.with_suffix('.json') if self.toml else self.cfgfile.with_suffix('.toml')
+        if convertee.exists():
+            log.info(f'{self.cfgfile} already exists as {convertee}; Loading and converting...')
+            loadfile = convertee
+            loadfunc = json.load if self.toml else toml.load
+        else:
+            loadfile = self.cfgfile
+            loadfunc = self.loadfunc
         try:
-            with open(self.cfgfile, 'r') as cf:
-                self.cfgs = self.loadfunc(cf)
-            log.info(f'{self.cfgfile} loaded.')
+            with open(loadfile, 'r') as cf:
+                self.cfgs = loadfunc(cf)
+            log.info(f'{loadfile} loaded.')
         except FileNotFoundError:
-            log.warning(f'{self.cfgfile} does not exist.')
+            log.warning(f'{loadfile} does not exist.')
             if self.default:
                 self._loadDefault()
-                log.info(f'Initiated {self.cfgfile} from {self.default}!')
+                log.info(f'Initiated {loadfile} from {self.default}!')
             else:
                 self.cfgs = {}
                 log.info('Loaded as empty dict!')
         except IOError:
-            log.critical(f'Could not load {self.cfgfile}!')
+            log.critical(f'Could not load {loadfile}!')
             self.cfgs = {}
             log.info('Loaded as empty dict!')
+        if convertee.exists():
+            self._save()
+            convertee.unlink()
+            log.info(f'Deleted {convertee}.')
 
     def _loadDefault(self):
         try:
@@ -55,10 +75,11 @@ class Config(MutableMapping):
             log.info('Loaded as empty dict!')
 
     def _save(self):
-        os.makedirs(os.path.dirname(self.cfgfile), exist_ok=True)
-        with open(f'{self.cfgfile}.tmp', 'w') as tmp:
+        self.cfgfile.parent.mkdir(parents=True, exist_ok=True)
+        tmpfile = self.cfgfile.with_suffix('.tmp')
+        with open(tmpfile, 'w') as tmp:
             self.dumpfunc(self.cfgs.copy(), tmp)
-        os.replace(f'{self.cfgfile}.tmp', self.cfgfile)
+        tmpfile.replace(self.cfgfile)
 
     async def save(self):
         with await self.lock:
