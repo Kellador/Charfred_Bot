df0015ae5c1f91cd5fc4d031ac6a8ae1ea5931be
Kellador <kellador@gmail.com>
2020-03-05 01:09:54 +0100

    :art: reduce code duplication

--- admincogs/gearbox.py
+++ admincogs/gearbox.py
@@ -50,6 +50,21 @@ class Gearbox(commands.Cog):
         else:
             return None
 
+    def _seek(self, cog, searchfunc, retryfunc, actionword):
+        candidates = searchfunc(cog)
+        if candidates:
+            if isinstance(candidates, list):
+                status = (False, f'Found multiple matching cogs: >\n' +
+                          "\n".join(candidates) +
+                          '\n< Please be more specific!')
+                log.info(f'Direct {actionword} failed, search inconclusive.')
+            else:
+                return retryfunc(candidates, search=False)
+        else:
+            status = (False, f'Could not {actionword} "{cog}", search yielded no matches!')
+            log.info(f'Direct {actionword} failed, search yielded no matches.')
+        return status
+
     def _load(self, cog, search=True):
         try:
             self.bot.load_extension(cog)
@@ -58,18 +73,7 @@ class Gearbox(commands.Cog):
             log.warning(status[1])
         except ExtensionNotFound:
             if search:
-                candidates = self._searchpaths(cog)
-                if candidates:
-                    if isinstance(candidates, list):
-                        status = (False, f'Found multiple matching cogs: >\n' +
-                                  "\n".join(candidates) +
-                                  '\n< Please be more specific!')
-                        log.info('Direct load failed, search inconclusive.')
-                    else:
-                        return self._load(candidates, search=False)
-                else:
-                    status = (False, f'Could not load "{cog}", search yielded no matches!')
-                    log.info('Direct load failed, search yielded no matches.')
+                status = self._seek(cog, self._searchpaths, self._load, 'load')
             else:
                 status = (False, f'Could not load "{cog}", not found!')
                 log.info(status[1])
@@ -90,18 +94,7 @@ class Gearbox(commands.Cog):
             self.bot.unload_extension(cog)
         except ExtensionNotLoaded:
             if search:
-                candidates = self._searchloaded(cog)
-                if candidates:
-                    if isinstance(candidates, list):
-                        status = (False, f'Found multiple matching cogs: >\n' +
-                                  "\n".join(candidates) +
-                                  '\n< Please be more specific!')
-                        log.info('Direct unload failed, search inconclusive.')
-                    else:
-                        return self._unload(candidates, search=False)
-                else:
-                    status = (False, f'Could not unload "{cog}", search yielded no matches!')
-                    log.info('Direct unload failed, search yielded no matches.')
+                status = self._seek(cog, self._searchloaded, self._unload, 'unload')
             else:
                 status = (True, f'Could not unload "{cog}", not loaded!')
                 log.warning(status[1])
@@ -113,37 +106,9 @@ class Gearbox(commands.Cog):
     def _reload(self, cog, search=True):
         try:
             self.bot.reload_extension(cog)
-        except ExtensionNotLoaded:
+        except (ExtensionNotLoaded, ExtensionNotFound):
             if search:
-                candidates = self._searchloaded(cog)
-                if candidates:
-                    if isinstance(candidates, list):
-                        status = (False, f'Found multiple matching cogs: >\n' +
-                                  "\n".join(candidates) +
-                                  '\n< Please be more specific!')
-                        log.info('Direct unload failed, search inconclusive.')
-                    else:
-                        return self._reload(candidates, search=False)
-                else:
-                    status = (False, f'Could not reload "{cog}", search yielded no matches!')
-                    log.info('Direct unload failed, search yielded no matches.')
-            else:
-                status = (False, f'Could not reload "{cog}", not loaded!')
-                log.warning(status[1])
-        except ExtensionNotFound:
-            if search:
-                candidates = self._searchloaded(cog)
-                if candidates:
-                    if isinstance(candidates, list):
-                        status = (False, f'Found multiple matching cogs: >\n' +
-                                  "\n".join(candidates) +
-                                  '\n< Please be more specific!')
-                        log.info('Direct unload failed, search inconclusive.')
-                    else:
-                        return self._reload(candidates, search=False)
-                else:
-                    status = (False, f'Could not reload "{cog}", search yielded no matches!')
-                    log.info('Direct unload failed, search yielded no matches.')
+                status = self._seek(cog, self._searchloaded, self._reload, 'reload')
             else:
                 status = (False, f'Could not reload "{cog}", not loaded!')
                 log.warning(status[1])
